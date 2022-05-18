ed26132bd437eceb09f9fe6ec76b0318d11e46d1
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-05-19 03:11:16 +0600

    install some packages

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,9 +2,39 @@ import React from 'react';
 
 const ToDoList = () => {
     return (
-        <div>
-            
+        <section>
+        <h1 class="text-center mt-5 mb-3">To Do List</h1>
+        <div class="container w-75 mx-auto text-center">
+          <div class="input-group">
+            <input
+            id="input_value"
+              class="form-control"
+              aria-label="With textarea"
+            ></input>
+            <button id="add_btn" class="btn btn-danger">Add Items</button>
+          </div>
+           {/* // table  */}
+          <div class="mt-5">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Number</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody id="content-container">
+                <tr id="table-content">
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Button</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+           {/* table  */}
         </div>
+      </section>
     );
 };
 
